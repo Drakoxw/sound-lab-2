677102c04f8b9ed89621c84ea6dabf1fa13dc8bd
Drakoxw <dragon12xw@gmail.com>
2024-02-07 22:03:20 -0500

    feat: home ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 
+const TITLE_DEFAULT = 'SoundLab - La casa del DJ';
+
 export const routes: Routes = [
   {
     path: '',
@@ -7,18 +9,18 @@ export const routes: Routes = [
     children: [
       {
         path: '',
-        title: 'Bienvenido a la casa del DJ',
+        title: TITLE_DEFAULT,
         loadComponent: () => import('@public/views/home/home.component')
       },
       {
         path: 'inicio',
-        title: 'Bienvenido a la casa del DJ',
+        title: TITLE_DEFAULT,
         loadComponent: () => import('@public/views/home/home.component')
       },
       {
-        path: 'contacto',
-        title: 'Bienvenido a la casa del DJ',
-        loadComponent: () => import('@public/views/home/home.component')
+        path: 'contactanos',
+        title: 'Contactanos via email',
+        loadComponent: () => import('@public/views/contact-me/contact-me.component')
       },
       {
         path: 'login',
