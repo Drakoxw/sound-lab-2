f4e7c12d3c05558b06ceedc84f03985d397b3f71
Drakoxw <dragon12xw@gmail.com>
2024-02-07 21:55:50 -0500

    feat: home ok

--- src/app/modules/shared/chat-me-whatsapp/chat-me-whatsapp.component.ts
+++ src/app/modules/shared/chat-me-whatsapp/chat-me-whatsapp.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WHATSAPP_LINK } from '@constants/index';
 
 @Component({
   selector: 'app-chat-me-whatsapp',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './chat-me-whatsapp.component.css'
 })
 export class ChatMeWhatsappComponent {
-
+  linkWhastapp = WHATSAPP_LINK;
 }
--- src/app/modules/shared/dashboard-public/dashboard-public.component.ts
+++ src/app/modules/shared/dashboard-public/dashboard-public.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ChatMeWhatsappComponent } from '@shared/chat-me-whatsapp/chat-me-whatsapp.component';
 import { NavBarComponent } from '@shared/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-dashboard-public',
   standalone: true,
-  imports: [RouterModule, NavBarComponent],
+  imports: [RouterModule, NavBarComponent, ChatMeWhatsappComponent],
   templateUrl: './dashboard-public.component.html',
   styleUrl: './dashboard-public.component.css'
 })
