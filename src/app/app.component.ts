405646625b3388dc69ee61f3918f95203f31d38a
Drakoxw <dragon12xw@gmail.com>
2024-02-08 01:00:30 -0500

    feat: menu dinamico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, PLATFORM_ID, inject } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { isPlatformBrowser } from '@angular/common';
+import { AuthService, LocalstorageService } from './services';
+import { MENU_KEY } from '@constants/index';
 
 
 @Component({
@@ -11,10 +13,32 @@ import { isPlatformBrowser } from '@angular/common';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  private localStorage = inject(LocalstorageService);
+  private authService = inject(AuthService);
+
   static isBrowser = new BehaviorSubject<boolean>(false);
 
-  constructor(@Inject(PLATFORM_ID) private platformId: any) {
+  constructor(@Inject(PLATFORM_ID) private platformId: any, private route: ActivatedRoute) {
     AppComponent.isBrowser.next(isPlatformBrowser(platformId));
   }
-  title = 'sound-lab';
+
+  ngOnInit(): void {
+    if (this.localStorage.getItem(MENU_KEY)) {
+      this.authService.updateMenuState(true);
+    }
+
+    this.route.queryParams.subscribe((params) => {
+      const menu = String(params['menu']);
+      switch (menu) {
+        case 'on':
+          this.authService.updateMenuState(true);
+          this.localStorage.setItem(MENU_KEY, String(Date.now()))
+          break;
+        case 'off':
+          this.authService.updateMenuState(false);
+          this.localStorage.removeItem(MENU_KEY);
+          break;
+      }
+    });
+  }
 }
